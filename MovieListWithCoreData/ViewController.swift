63eb52df01fd4e199f67fa33b2d653bede1364db
hackeru <hackeru@hackerus-Mac.local>
2019-04-30 09:44:48 -0700

    Initial Commit

---
+++ MovieListWithCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieListWithCoreData
+//
+//  Created by hackeru on 30/04/2019.
+//  Copyright © 2019 erez8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
